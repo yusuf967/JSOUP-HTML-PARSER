0ac5d82fa7114aa5ed57b9d04758abb6a2cfe126
Yusuf Doğan <58945471+yusuf967@users.noreply.github.com>
2021-11-09 23:39:11 +0300

    Update vatan.java

--- staj_proje/src/staj_proje/vatan.java
+++ staj_proje/src/staj_proje/vatan.java
@@ -11,7 +11,7 @@ import org.jsoup.select.Elements;
 
 public class vatan {
 
-    static int sayac = 1049;
+    static int sayac = 0;
 
     public void vatanBaglanEkranKart(String url) {
         for (int j = 1; j < 8; j++) {
